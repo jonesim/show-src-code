2dcb4086ed888d8ffd89d08e7ce7650cb22a735c
jonesim <ian@cobi.co.uk>
2021-09-19 18:37:59 +0100

    Add PypiAppConfig

--- show_src_code/apps.py
+++ show_src_code/apps.py
@@ -1,4 +1,14 @@
+import json
+import requests
 from django.apps import AppConfig
+
+
+class PypiAppConfig(AppConfig):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        r = requests.get('https://pypi.org/pypi/{}/json'.format(self.pypi))
+        self.pypi_data = json.loads(r.text)
 
 
 class SrcCodeConfig(AppConfig):
