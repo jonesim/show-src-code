d725d9df63266e74435a8748c4b1ac8a8c52e7c1
jonesim <ian@cobi.co.uk>
2021-09-20 09:08:17 +0100

    Fix namespace issue

--- show_src_code/urls.py
+++ show_src_code/urls.py
@@ -3,9 +3,12 @@ from django.urls import path, include
 
 from .modals import BaseSourceCodeModal
 
-app_name = 'show_src_code'
-urlpatterns = [
+local_patterns = [
     path('modal/source_code/<str:slug>/', BaseSourceCodeModal.as_view(), name='source_code_modal'),
+]
+
+urlpatterns = [
+    path('', include((local_patterns, 'show_src_code'))),
     # Load urls file from app if urls is setup in AppConfig
-    *[path('', include(a.urls)) for a in apps.get_app_configs() if hasattr(a, 'urls')]
+    *[path('', include((a.urls, ''),)) for a in apps.get_app_configs() if hasattr(a, 'urls')]
 ]
