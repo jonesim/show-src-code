ac7ab1a2c3ef6b39428e3d66589f38a6cad491a6
jonesim <ian@cobi.co.uk>
2021-09-20 08:42:12 +0100

    Import urls from configured apps

--- show_src_code/urls.py
+++ show_src_code/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.apps import apps
+from django.urls import path, include
 
 from .modals import BaseSourceCodeModal
 
 app_name = 'show_src_code'
 urlpatterns = [
     path('modal/source_code/<str:slug>/', BaseSourceCodeModal.as_view(), name='source_code_modal'),
+    # Load urls file from app if urls is setup in AppConfig
+    *[path('', include(a.urls)) for a in apps.get_app_configs() if hasattr(a, 'urls')]
 ]
